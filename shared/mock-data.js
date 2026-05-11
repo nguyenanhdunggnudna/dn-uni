f930d5e862150672bbd34aa3cd75b056d46437d5
Dung <nguyenanhdunggnudna2@gmail.com>
2026-05-11 11:57:57 +0700

    feat: update UI

--- shared/mock-data.js
+++ shared/mock-data.js
@@ -24,6 +24,20 @@ window.MOCK_DATA = {
             UserAvatar: "../assets/images/avatar.svg",
             DashboardTitle: "Hệ thống Văn thư",
             Permissions: ["INCOMING_DOCS", "OUTGOING_DOCS"]
+        },
+        "AdministrativeHead": {
+            UserName: "Nguyễn Văn A",
+            UserRole: "Trưởng phòng hành chính",
+            UserAvatar: "../assets/images/avatar.svg",
+            DashboardTitle: "Hệ thống Quản lý Hành chính",
+            Permissions: ["INTERNAL_NEWS", "INCOMING_DOCS", "OUTGOING_DOCS"]
+        },
+        "DeputyHead": {
+            UserName: "Phạm Văn B",
+            UserRole: "Phó phòng",
+            UserAvatar: "../assets/images/avatar.svg",
+            DashboardTitle: "Hệ thống Quản lý Phó phòng",
+            Permissions: ["INTERNAL_NEWS", "INCOMING_DOCS", "OUTGOING_DOCS", "TASKS", "CALENDAR", "UTILITIES"]
         }
     },
     currentRole: localStorage.getItem('userRole') || 'Director'
